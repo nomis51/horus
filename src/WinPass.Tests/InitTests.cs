da6e9ebdb4ea0eead1b2adfb879a27e1d83828c3
nomis51 <nomis51@users.noreply.github.com>
2023-06-14 21:12:47 -0400

    (wip) tests

---
+++ src/WinPass.Tests/InitTests.cs
@@ -0,0 +1,38 @@
+﻿namespace WinPass.Tests;
+
+public class InitTests
+{
+    #region Members
+
+    private readonly Cli _sut;
+
+    #endregion
+
+    #region Constructors
+
+    public InitTests()
+    {
+        _sut = new Cli();
+    }
+
+    #endregion
+
+    #region Tests
+
+    [Fact]
+    public void ShouldInitialiazeStore()
+    {
+    }
+
+    [Fact]
+    public void ShouldErrorWithStoreAlreadyInitialized()
+    {
+    }
+
+    [Fact]
+    public void ShouldErrorWithGpgIdMissing()
+    {
+    }
+
+    #endregion
+}
