ec4109c1fe0462600182559d5af9e9287b3f5a3b
nomis51 <nomis51@users.noreply.github.com>
2023-06-15 15:59:46 -0400

    (wip)

--- src/WinPass.Tests/InitTests.cs
+++ src/WinPass.Tests/InitTests.cs
@@ -1,10 +1,16 @@
-﻿namespace WinPass.Tests;
+﻿using System.Reflection;
+using System.Text;
+using Xunit.Abstractions;
+
+namespace WinPass.Tests;
 
 public class InitTests
 {
     #region Members
 
     private readonly Cli _sut;
+    private readonly StringBuilder _stdout;
+    private const string GpgId = "F7153DF773DBD9AD";
 
     #endregion
 
@@ -13,6 +19,8 @@ public class InitTests
     public InitTests()
     {
         _sut = new Cli();
+        _stdout = new StringBuilder();
+        Console.SetOut(new StringWriter(_stdout));
     }
 
     #endregion
@@ -20,8 +28,17 @@ public class InitTests
     #region Tests
 
     [Fact]
-    public void ShouldInitialiazeStore()
+    public void ShouldInitializeStore()
     {
+        // Arrange
+        var method = _sut.GetType().GetMethod("Init", BindingFlags.NonPublic | BindingFlags.Instance);
+        Assert.NotNull(method);
+        
+        // Act
+        method!.Invoke(_sut, new object[] { GpgId });
+
+        // Assert
+        var g = 0;
     }
 
     [Fact]
