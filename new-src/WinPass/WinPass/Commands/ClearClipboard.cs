71813d4d098b52c3be45ebf3472e6166179d0b64
nomis51 <nomis51@users.noreply.github.com>
2023-08-10 20:20:59 -0400

    Refactor commands

---
+++ new-src/WinPass/WinPass/Commands/ClearClipboard.cs
@@ -0,0 +1,20 @@
+﻿using WinPass.Commands.Abstractions;
+using WinPass.Shared.Helpers;
+
+namespace WinPass.Commands;
+
+public class ClearClipboard : ICommand
+{
+    #region Public methods
+
+    public void Run(List<string> args)
+    {
+        if (args.Count == 0) return;
+        if (!int.TryParse(args[0], out var intValue)) return;
+
+        Thread.Sleep(1000 * intValue);
+        ClipboardHelper.Clear();
+    }
+
+    #endregion
+}
---
+++ new-src/WinPass/WinPass/Commands/Generate.cs
@@ -0,0 +1,75 @@
+﻿using Spectre.Console;
+using WinPass.Commands.Abstractions;
+using WinPass.Core.Services;
+using WinPass.Shared;
+using WinPass.Shared.Models.Errors.Fs;
+
+namespace WinPass.Commands;
+
+public class Generate : ICommand
+{
+    #region Public methods
+
+    public void Run(List<string> args)
+    {
+        if (!Cli.AcquireLock()) return;
+        if (!AppService.Instance.IsStoreInitialized())
+        {
+            AnsiConsole.MarkupLine($"[red]{new FsStoreNotInitializedError().Message}[/]");
+            return;
+        }
+
+        if (args.Count == 0)
+        {
+            AnsiConsole.MarkupLine($"[red]{Locale.Get("cli.args.passwordNameRequired")}[/]");
+            return;
+        }
+
+        var (settings, errorSettings) = AppService.Instance.GetSettings();
+        if (errorSettings is not null)
+        {
+            AnsiConsole.MarkupLine(
+                $"[yellow]{Locale.Get("error.loadingSettings", new object[] { errorSettings.Message })}[/]");
+        }
+
+        var name = args[^1];
+        var length = settings?.DefaultLength ?? 0;
+        var customAlphabet = settings?.DefaultCustomAlphabet ?? string.Empty;
+
+        for (var i = 0; i < args.Count - 1; ++i)
+        {
+            if (args[i].StartsWith("-s="))
+            {
+                var value = args[i].Split("=").Last();
+                if (!int.TryParse(value, out var intValue)) continue;
+
+                length = intValue;
+            }
+
+            if (args[i].StartsWith("-a="))
+            {
+                var value = args[i].Split("=").Last();
+                if (string.IsNullOrWhiteSpace(value)) continue;
+
+                customAlphabet = value;
+            }
+        }
+
+        var resultGenerateNewPassword = AppService.Instance.GenerateNewPassword(
+            name,
+            length,
+            customAlphabet
+        );
+
+        if (resultGenerateNewPassword.HasError)
+        {
+            AnsiConsole.MarkupLine(
+                $"[{Cli.GetErrorColor(resultGenerateNewPassword.Error!.Severity)}]{resultGenerateNewPassword.Error!.Message}[/]");
+            return;
+        }
+
+        AnsiConsole.MarkupLine(Locale.Get("passwordGenerated", new object[] { name }));
+    }
+
+    #endregion
+}
