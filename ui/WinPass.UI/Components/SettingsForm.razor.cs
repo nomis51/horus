fea761fe4c9ae28055c13caa47990d9da6ea9954
nomis51 <nomis51@users.noreply.github.com>
2023-09-27 13:29:33 -0400

    (wip) layout settings form

--- ui/WinPass.UI/Components/SettingsForm.razor.cs
+++ ui/WinPass.UI/Components/SettingsForm.razor.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using WinPass.Core.Services;
+using WinPass.Shared.Models.Data;
 using WinPass.UI.Components.Abstractions;
 
 namespace WinPass.UI.Components;
@@ -7,11 +12,11 @@ public class SettingsFormBase : Component
 {
     #region Constants
 
-    protected readonly string[] Languages = new[]
+    protected readonly Dictionary<string, string> Languages = new()
     {
-        "English",
-        "French",
-        "German",
+        { "en", "English" },
+        { "fr", "French" },
+        { "de", "German" },
     };
 
     #endregion
@@ -20,6 +25,38 @@ public class SettingsFormBase : Component
 
     [Parameter]
     public EventCallback OnClose { get; set; }
+
+    #endregion
+
+    #region Members
+
+    protected Settings Settings { get; private set; }
+    protected int GpgCacheTtl { get; set; }
+
+    #endregion
+
+    #region Lifecycle
+
+    protected override void OnInitialized()
+    {
+        RetrieveSettings();
+    }
+
+    #endregion
+
+    #region Private methods
+
+    private void RetrieveSettings()
+    {
+        var (settings, error) = AppService.Instance.GetSettings();
+        if (error is not null || settings is null)
+        {
+            Snackbar.Add($"Unable to retrieve settings: {error!.Message}", Severity.Error);
+            return;
+        }
+
+        Settings = settings;
+    }
 
     #endregion
 }
