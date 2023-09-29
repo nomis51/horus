3e39ef7e98815f0c2485bc12e390af6ea4f602df
nomis51 <nomis51@users.noreply.github.com>
2023-09-28 20:31:55 -0400

    Add form and start page to initialize store

---
+++ ui/WinPass.UI/Components/InitStoreForm.razor.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using WinPass.Core.Services;
+using WinPass.UI.Components.Abstractions;
+
+namespace WinPass.UI.Components;
+
+public class InitStoreFormBase : Component
+{
+    #region Parameters
+
+    [Parameter]
+    public EventCallback OnClose { get; set; }
+
+    #endregion
+
+    #region Members
+
+    protected string GpgId { get; private set; } = string.Empty;
+    protected string GitUrl { get; private set; } = string.Empty;
+    protected bool IsGpgIdValid { get; private set; } = true;
+    protected bool IsLoading { get; private set; }
+
+    #endregion
+
+    #region Protected methods
+
+    protected void Create()
+    {
+        IsLoading = true;
+        Task.Run(async () =>
+        {
+            var result = AppService.Instance.InitializeStoreFolder(GpgId, GitUrl);
+            if (result.HasError)
+            {
+                Snackbar.Add($"Failed to initialize store: {result.Error!.Message}", Severity.Error);
+                return;
+            }
+
+            Snackbar.Add("Store initialized successfully", Severity.Success);
+
+            await InvokeAsync(async () =>
+            {
+                IsLoading = false;
+                await OnClose.InvokeAsync();
+            });
+        });
+    }
+
+    protected void Quit()
+    {
+        Environment.Exit(0);
+    }
+
+    protected void SetGpgId(string value)
+    {
+        GpgId = value;
+    }
+
+    protected void SetGitHubUrl(string value)
+    {
+        GitUrl = value;
+    }
+
+    #endregion
+}
---
+++ ui/WinPass.UI/Pages/StartPage.razor.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using WinPass.Core.Services;
+using WinPass.UI.Components;
+using WinPass.UI.Components.Abstractions;
+
+namespace WinPass.UI.Pages;
+
+public class StartPageBase : Component
+{
+    #region Services
+
+    [Inject]
+    protected NavigationManager NavigationManager { get; set; }
+
+    #endregion
+
+    #region Members
+
+    private IDialogReference? _initStoreFormDialogReference;
+
+    #endregion
+
+    #region Lifecycle
+
+    protected override void OnInitialized()
+    {
+        EnsureStoreInitialized();
+    }
+
+    #endregion
+
+    #region Private methods
+
+    private void EnsureStoreInitialized()
+    {
+        if (!AppService.Instance.IsStoreInitialized())
+        {
+            _initStoreFormDialogReference = DialogService.Show<InitStoreForm>("Initialize store",
+                new DialogParameters
+                {
+                    {
+                        "OnClose",
+                        EventCallback.Factory.Create(this, () =>
+                        {
+                            _initStoreFormDialogReference?.Close();
+                            NavigationManager.NavigateTo("/home");
+                        })
+                    }
+                }, new DialogOptions
+                {
+                    CloseButton = false,
+                    CloseOnEscapeKey = false,
+                    DisableBackdropClick = true,
+                    MaxWidth = MaxWidth.Large,
+                    FullWidth = true
+                });
+        }
+        else
+        {
+            NavigationManager.NavigateTo("/home");
+        }
+    }
+
+    #endregion
+}
