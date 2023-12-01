866f8e7a5a263e253219c5e0ea681556c9061e3f
nomis51 <nomis51@users.noreply.github.com>
2023-12-01 18:58:04 -0500

    Add settings dialog

--- ui/Horus.UI/ViewModels/SettingsDialogViewModel.cs
+++ ui/Horus.UI/ViewModels/SettingsDialogViewModel.cs
@@ -1,6 +1,70 @@
-﻿namespace Horus.UI.ViewModels;
+﻿using System;
+using Horus.Core.Services;
+using Horus.Shared.Models.Data;
+using Horus.UI.Enums;
+using Horus.UI.Services;
+using ReactiveUI;
 
-public class SettingsViewModel : ViewModelBase
+namespace Horus.UI.ViewModels;
+
+public class SettingsDialogViewModel : ViewModelBase
 {
-    
+    #region Props
+
+    public Settings Settings { get; private set; } = new();
+
+    private bool _isLoading;
+
+    public bool IsLoading
+    {
+        get => _isLoading;
+        set => this.RaiseAndSetIfChanged(ref _isLoading, value);
+    }
+
+    #endregion
+
+    #region Constructors
+
+    public SettingsDialogViewModel()
+    {
+        RetrieveSettings();
+    }
+
+    #endregion
+
+    #region Public methods
+
+    public bool SaveSettings()
+    {
+        IsLoading = true;
+        var result = AppService.Instance.SaveSettings(Settings);
+        IsLoading = false;
+
+        if (result.HasError)
+        {
+            SnackbarService.Instance.Show("Failed to save settings", SnackbarSeverity.Error, 5000);
+            return false;
+        }
+
+        SnackbarService.Instance.Show("Settings saved", SnackbarSeverity.Success);
+        return true;
+    }
+
+    #endregion
+
+    #region Private methods
+
+    private void RetrieveSettings()
+    {
+        var (settings, error) = AppService.Instance.GetSettings();
+        if (error is not null)
+        {
+            SnackbarService.Instance.Show("Failed to retrieve settings", SnackbarSeverity.Error, 5000);
+            return;
+        }
+
+        Settings = settings!;
+    }
+
+    #endregion
 }
--- ui/Horus.UI/Views/Dialogs/SettingsDialog.axaml.cs
+++ ui/Horus.UI/Views/Dialogs/SettingsDialog.axaml.cs
@@ -1,10 +1,11 @@
-﻿using Avalonia.Interactivity;
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Horus.UI.Abstractions;
 using Horus.UI.ViewModels;
 
 namespace Horus.UI.Views.Dialogs;
 
-public partial class SettingsDialog : DialogView<SettingsViewModel>
+public partial class SettingsDialog : DialogView<SettingsDialogViewModel>
 {
     #region Constructors
 
@@ -24,11 +25,37 @@ public partial class SettingsDialog : DialogView<SettingsViewModel>
 
     private void ButtonSave_OnClick(object? sender, RoutedEventArgs e)
     {
+        Dispatch(vm =>
+        {
+            if (!vm!.SaveSettings()) return;
+
+            InvokeUi(() => OnClose());
+        });
     }
 
     private void ButtonClose_OnClick(object? sender, RoutedEventArgs e)
     {
         OnClose();
+    }
+
+    private void TextBoxPasswordLength_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        ViewModel!.Settings.DefaultLength = int.TryParse(TextBoxPasswordLength.Text, out var intValue) && intValue >= 0 ? intValue : 0;
+    }
+
+    private void TextBoxPasswordAlphabet_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        ViewModel!.Settings.DefaultCustomAlphabet = TextBoxPasswordAlphabet.Text ?? string.Empty;
+    }
+
+    private void TextBoxClearTimeout_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        ViewModel!.Settings.ClearTimeout = int.TryParse(TextBoxClearTimeout.Text, out var intValue) && intValue >= 0 ? intValue : 0;
+    }
+
+    private void TextBoxAutoFetchInterval_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        ViewModel!.Settings.FetchInterval = int.TryParse(TextBoxAutoFetchInterval.Text, out var intValue) && intValue >= 0 ? intValue : 0;
     }
 
     #endregion
