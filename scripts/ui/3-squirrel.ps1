6dbfefc03de9dbbbe62ef79801f65a02728c4402
nomis51 <nomis51@users.noreply.github.com>
2023-12-03 15:54:53 -0500

    Update splash and doc image

--- scripts/ui/3-squirrel.ps1
+++ scripts/ui/3-squirrel.ps1
@@ -2,4 +2,4 @@ param(
 	[Parameter(Mandatory=$true)][string]$version
 )
 
-~\.nuget\packages\clowd.squirrel\2.11.1\tools\squirrel.exe pack --icon=../../ui/Horus/Assets/logo.ico --appIcon=../../ui/Horus/Assets/logo.ico --splashImage=../../ui/Horus/Assets/logo.gif --framework=net8 --packTitle=Horus --packDir=../../ui/Horus/bin/Release/net8.0/ --packId=Horus --packVersion=$version --allowUnaware --packAuthors=nomis51
+~\.nuget\packages\clowd.squirrel\2.11.1\tools\squirrel.exe pack --icon=../../ui/Horus/Assets/logo.ico --appIcon=../../ui/Horus/Assets/logo.ico --splashImage=../../ui/Horus/Assets/splash.gif --framework=net8 --packTitle=Horus --packDir=../../ui/Horus/bin/Release/net8.0/ --packId=Horus --packVersion=$version --allowUnaware --packAuthors=nomis51
