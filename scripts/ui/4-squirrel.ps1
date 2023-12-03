78cf92655838a207587a19603e9ef7815e20c52e
nomis51 <nomis51@users.noreply.github.com>
2023-12-03 11:05:10 -0500

    Version update 2.9.0

--- scripts/ui/4-squirrel.ps1
+++ scripts/ui/4-squirrel.ps1
@@ -1,1 +1,1 @@
-~\.nuget\packages\squirrel.windows\1.4.4\tools\squirrel.exe --releasify=./Horus.nupkg --no-msi --setupIcon=../../ui/Horus/Assets/logo.ico
+~\.nuget\packages\squirrel.windows\1.4.4\tools\squirrel.exe --releasify=./Horus.nupkg --no-msi --setupIcon=../../ui/Horus/Assets/logo.ico --loadingGif=../../ui/Horus/Assets/logo.gif
