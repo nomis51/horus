361b9c3a8b4adde091b8c00079289e1bbff29bf7
nomis51 <nomis51@users.noreply.github.com>
2023-12-27 11:47:49 -0500

    FIX: script for windows portable building

--- scripts/ui/4-package-portable.ps1
+++ scripts/ui/4-package-portable.ps1
@@ -1,2 +1,2 @@
 rm -fo ./Releases/HorusPortable.zip
-Compress-Archive -Path ../../ui/Horus/bin/Release/net8.0/* -DestinationPath ./Releases/HorusPortable.zip
+Compress-Archive -Path ../../ui/Horus/bin/Release/net8.0/* -DestinationPath ./Releases/HorusPortable-win.zip
